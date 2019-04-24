26bb01d656dd11a392f4e77f37bfd0ca76f9c1d5
Dimple <bianxiaofeng@sohu.com>
2019-04-24 11:01:04 +0800

    添加最新SQL文件、修改Tag搜索失效的bug

--- src/main/java/com/dimple/project/blog/tag/service/impl/TagServiceImpl.java
+++ src/main/java/com/dimple/project/blog/tag/service/impl/TagServiceImpl.java
@@ -1,6 +1,5 @@
 package com.dimple.project.blog.tag.service.impl;
 
-import com.dimple.common.constant.CachePrefix;
 import com.dimple.common.constant.TagConstants;
 import com.dimple.common.utils.security.ShiroUtils;
 import com.dimple.project.blog.blog.mapper.BlogTagMapper;
@@ -8,7 +7,6 @@ import com.dimple.project.blog.tag.domain.Tag;
 import com.dimple.project.blog.tag.mapper.TagMapper;
 import com.dimple.project.blog.tag.service.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -31,7 +29,6 @@ public class TagServiceImpl implements TagService {
     @Autowired
     BlogTagMapper blogTagMapper;
 
-    @Cacheable(value = CachePrefix.FRONT_TAG)
     @Override
     public List<Tag> selectTagList(Tag tag) {
         List<Tag> tags = tagMapper.selectTagList(tag);
