49ed1fe86e3d9f5f9ed69449bfedebdfaddd01a5
Dimple <bianxiaofeng@sohu.com>
2019-11-26 22:54:49 +0800

    添加代码高亮

--- src/main/gui/src/front/components/content/ArticleContent.vue
+++ src/main/gui/src/front/components/content/ArticleContent.vue
@@ -8,7 +8,7 @@
           <article-page-content v-highlight>
             <div class="article-details" id="article-main-page" slot="content" ref="article">
               <div class="detail" v-if="article">
-                <article class="typo container article-main-content" v-html="article.content">
+                <article class="typo container article-main-content" v-highlight v-html="article.content">
                 </article>
                 <div class="detail-footer">
                   以上内容添加于 {{ article.createTime | socialDate }} &nbsp;&nbsp;&nbsp;
