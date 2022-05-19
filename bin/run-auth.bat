f08c41d0cec34665d3ebb0a8bb86e51a13eafca0
Dimple <bianxiaofeng@sohu.com>
2022-05-19 16:08:07 +0800

    update some config file

--- bin/run-auth.bat
+++ bin/run-auth.bat
@@ -4,11 +4,11 @@ echo [信息] 使用Jar命令运行Auth工程。
 echo.
 
 cd %~dp0
-cd ../ruoyi-auth/target
+cd ../dimple-auth/target
 
 set JAVA_OPTS=-Xms512m -Xmx1024m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m
 
-java -Dfile.encoding=utf-8 %JAVA_OPTS% -jar ruoyi-auth.jar
+java -Dfile.encoding=utf-8 %JAVA_OPTS% -jar dimple-auth.jar
 
 cd bin
 pause
--- bin/run-gateway.bat
+++ bin/run-gateway.bat
@@ -4,11 +4,11 @@ echo [信息] 使用Jar命令运行Gateway工程。
 echo.
 
 cd %~dp0
-cd ../ruoyi-gateway/target
+cd ../dimple-gateway/target
 
 set JAVA_OPTS=-Xms512m -Xmx1024m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m
 
-java -Dfile.encoding=utf-8 %JAVA_OPTS% -jar ruoyi-gateway.jar
+java -Dfile.encoding=utf-8 %JAVA_OPTS% -jar dimple-gateway.jar
 
 cd bin
 pause
