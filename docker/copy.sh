9fcb0829fb2f6f3c46e234ff71e7c9e0338ed39c
Martin <bianxiaofeng@sohu.com>
2022-12-09 10:21:39 +0800

    update the copy script

--- docker/copy.sh
+++ docker/copy.sh
@@ -9,8 +9,7 @@ usage() {
 
 # copy sql
 echo "begin copy sql "
-cp ../sql/dimple_20210908.sql ./mysql/db
-cp ../sql/dimple_config_20220114.sql ./mysql/db
+cp ../sql/*.sql ./mysql/db
 
 # copy html
 echo "begin copy html "
@@ -19,23 +18,19 @@ cp -r ../dimple-ui/dist/** ./nginx/html/dist
 
 # copy jar
 echo "begin copy dimple-gateway "
-cp ../dimple-gateway/target/dimple-gateway.jar ./ruoyi/gateway/jar
+cp ../dimple-gateway/target/dimple-gateway.jar ./dimple/gateway/jar
 
 echo "begin copy dimple-auth "
-cp ../dimple-auth/target/dimple-auth.jar ./ruoyi/auth/jar
+cp ../dimple-auth/target/dimple-auth.jar ./dimple/auth/jar
 
 echo "begin copy dimple-visual "
-cp ../dimple-visual/dimple-monitor/target/dimple-visual-monitor.jar  ./ruoyi/visual/monitor/jar
+cp ../dimple-visual/dimple-monitor/target/dimple-visual-monitor.jar  ./dimple/visual/monitor/jar
 
 echo "begin copy dimple-modules-system "
-cp ../dimple-modules/dimple-system/target/dimple-modules-system.jar ./ruoyi/modules/system/jar
+cp ../dimple-modules/dimple-system/target/dimple-modules-system.jar ./dimple/modules/system/jar
 
 echo "begin copy dimple-modules-file "
-cp ../dimple-modules/dimple-file/target/dimple-modules-file.jar ./ruoyi/modules/file/jar
+cp ../dimple-modules/dimple-file/target/dimple-modules-file.jar ./dimple/modules/file/jar
 
 echo "begin copy dimple-modules-job "
-cp ../dimple-modules/dimple-job/target/dimple-modules-job.jar ./ruoyi/modules/job/jar
-
-echo "begin copy dimple-modules-gen "
-cp ../dimple-modules/dimple-gen/target/dimple-modules-gen.jar ./ruoyi/modules/gen/jar
-
+cp ../dimple-modules/dimple-job/target/dimple-modules-job.jar ./dimple/modules/job/jar
