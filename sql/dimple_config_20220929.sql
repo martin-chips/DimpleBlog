e47fc298c087103700ec0cde9b81c7e218fc6c77
Martin <bianxiaofeng@sohu.com>
2022-12-11 15:41:37 +0800

    update nacos config sql and bootstrap.yml file nacos reference

--- sql/dimple_config_20220929.sql
+++ sql/dimple_config_20220929.sql
@@ -1,15 +1,15 @@
 DROP
-DATABASE IF EXISTS `ry-config`;
+DATABASE IF EXISTS `dimple-config`;
 
 CREATE
-DATABASE  `ry-config` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+DATABASE  `dimple-config` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
 SET NAMES utf8mb4;
 SET
 FOREIGN_KEY_CHECKS = 0;
 
 USE
-`ry-config`;
+`dimple-config`;
 
 /******************************************/
 /*   表名称 = config_info   */
@@ -37,44 +37,277 @@ CREATE TABLE `config_info`
     UNIQUE KEY `uk_configinfo_datagrouptenant` (`data_id`,`group_id`,`tenant_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='config_info';
 
-insert into config_info(id, data_id, group_id, content, md5, gmt_create, gmt_modified, src_user, src_ip, app_name,
-                        tenant_id, c_desc, c_use, effect, type, c_schema, encrypted_data_key)
-values (1, 'application-dev.yml', 'DEFAULT_GROUP',
-        'spring:\n  autoconfigure:\n    exclude: com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure\n  mvc:\n    pathmatch:\n      matching-strategy: ant_path_matcher\n\n# feign 配置\nfeign:\n  sentinel:\n    enabled: true\n  okhttp:\n    enabled: true\n  httpclient:\n    enabled: false\n  client:\n    config:\n      default:\n        connectTimeout: 10000\n        readTimeout: 10000\n  compression:\n    request:\n      enabled: true\n    response:\n      enabled: true\n\n# 暴露监控端点\nmanagement:\n  endpoints:\n    web:\n      exposure:\n        include: \' *\'\n',
-        'aaa73b809cfd4d0058893aa13da57806', '2020-05-20 12:00:00', '2022-04-24 10:26:34', 'nacos', '0:0:0:0:0:0:0:1',
-        '', '', '通用配置', 'null', 'null', 'yaml', NULL, ''),
-       (2, 'ruoyi-gateway-dev.yml', 'DEFAULT_GROUP',
-        'spring:\n  redis:\n    host: localhost\n    port: 6379\n    password:\n  cloud:\n    gateway:\n      discovery:\n        locator:\n          lowerCaseServiceId: true\n          enabled: true\n      routes:\n        # 认证中心\n        - id: ruoyi-auth\n          uri: lb://ruoyi-auth\n          predicates:\n            - Path=/auth/**\n          filters:\n            # 验证码处理\n            - CacheRequestFilter\n            - ValidateCodeFilter\n            - StripPrefix=1\n        # 代码生成\n        - id: ruoyi-gen\n          uri: lb://ruoyi-gen\n          predicates:\n            - Path=/code/**\n          filters:\n            - StripPrefix=1\n        # 定时任务\n        - id: ruoyi-job\n          uri: lb://ruoyi-job\n          predicates:\n            - Path=/schedule/**\n          filters:\n            - StripPrefix=1\n        # 系统模块\n        - id: ruoyi-system\n          uri: lb://ruoyi-system\n          predicates:\n            - Path=/system/**\n          filters:\n            - StripPrefix=1\n        # 文件服务\n        - id: ruoyi-file\n          uri: lb://ruoyi-file\n          predicates:\n            - Path=/file/**\n          filters:\n            - StripPrefix=1\n\n# 安全配置\nsecurity:\n  # 验证码\n  captcha:\n    enabled: true\n    type: math\n  # 防止XSS攻击\n  xss:\n    enabled: true\n    excludeUrls:\n      - /system/notice\n  # 不校验白名单\n  ignore:\n    whites:\n      - /auth/logout\n      - /auth/login\n      - /auth/register\n      - /*/v2/api-docs\n      - /csrf\n',
-        '57cec5abd0e0a6b77d853750344a9dc0', '2020-05-14 14:17:55', '2022-09-29 02:48:32', 'nacos', '0:0:0:0:0:0:0:1',
-        '', '', '网关模块', 'null', 'null', 'yaml', '', ''),
-       (3, 'ruoyi-auth-dev.yml', 'DEFAULT_GROUP',
-        'spring:\n  redis:\n    host: localhost\n    port: 6379\n    password:\n', '8bd9dada9a94822feeab40de55efced6',
-        '2020-11-20 00:00:00', '2022-09-29 02:48:42', 'nacos', '0:0:0:0:0:0:0:1', '', '', '认证中心', 'null', 'null',
-        'yaml', '', ''),
-       (4, 'ruoyi-monitor-dev.yml', 'DEFAULT_GROUP',
-        '# spring\nspring:\n  security:\n    user:\n      name: ruoyi\n      password: 123456\n  boot:\n    admin:\n      ui:\n        title: 若依服务状态监控\n',
-        '6f122fd2bfb8d45f858e7d6529a9cd44', '2020-11-20 00:00:00', '2022-09-29 02:48:54', 'nacos', '0:0:0:0:0:0:0:1',
-        '', '', '监控中心', 'null', 'null', 'yaml', '', ''),
-       (5, 'ruoyi-system-dev.yml', 'DEFAULT_GROUP',
-        '# spring配置\nspring:\n  redis:\n    host: localhost\n    port: 6379\n    password:\n  datasource:\n    druid:\n      stat-view-servlet:\n        enabled: true\n        loginUsername: admin\n        loginPassword: 123456\n    dynamic:\n      druid:\n        initial-size: 5\n        min-idle: 5\n        maxActive: 20\n        maxWait: 60000\n        timeBetweenEvictionRunsMillis: 60000\n        minEvictableIdleTimeMillis: 300000\n        validationQuery: SELECT 1 FROM DUAL\n        testWhileIdle: true\n        testOnBorrow: false\n        testOnReturn: false\n        poolPreparedStatements: true\n        maxPoolPreparedStatementPerConnectionSize: 20\n        filters: stat,slf4j\n        connectionProperties: druid.stat.mergeSql\\=true;druid.stat.slowSqlMillis\\=5000\n      datasource:\n          # 主库数据源\n          master:\n            driver-class-name: com.mysql.cj.jdbc.Driver\n            url: jdbc:mysql://localhost:3306/ry-cloud?useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&useSSL=true&serverTimezone=GMT%2B8\n            username: root\n            password: password\n          # 从库数据源\n          # slave:\n            # username: \n            # password: \n            # url: \n            # driver-class-name: \n\n# mybatis配置\nmybatis:\n    # 搜索指定包别名\n    typeAliasesPackage: com.ruoyi.system\n    # 配置mapper的扫描，找到所有的mapper.xml映射文件\n    mapperLocations: classpath:mapper/**/*.xml\n\n# swagger配置\nswagger:\n  title: 系统模块接口文档\n  license: Powered By ruoyi\n  licenseUrl: https://ruoyi.vip',
-        '48e0ed4a040c402bdc2444213a82c910', '2020-11-20 00:00:00', '2022-09-29 02:49:09', 'nacos', '0:0:0:0:0:0:0:1',
-        '', '', '系统模块', 'null', 'null', 'yaml', '', ''),
-       (6, 'ruoyi-gen-dev.yml', 'DEFAULT_GROUP',
-        '# spring配置\nspring:\n  redis:\n    host: localhost\n    port: 6379\n    password:\n  datasource:\n    driver-class-name: com.mysql.cj.jdbc.Driver\n    url: jdbc:mysql://localhost:3306/ry-cloud?useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&useSSL=true&serverTimezone=GMT%2B8\n    username: root\n    password: password\n\n# mybatis配置\nmybatis:\n    # 搜索指定包别名\n    typeAliasesPackage: com.ruoyi.gen.domain\n    # 配置mapper的扫描，找到所有的mapper.xml映射文件\n    mapperLocations: classpath:mapper/**/*.xml\n\n# swagger配置\nswagger:\n  title: 代码生成接口文档\n  license: Powered By ruoyi\n  licenseUrl: https://ruoyi.vip\n\n# 代码生成\ngen:\n  # 作者\n  author: ruoyi\n  # 默认生成包路径 system 需改成自己的模块名称 如 system monitor tool\n  packageName: com.ruoyi.system\n  # 自动去除表前缀，默认是false\n  autoRemovePre: false\n  # 表前缀（生成类名不会包含表前缀，多个用逗号分隔）\n  tablePrefix: sys_\n',
-        'eb592420b3fceae1402881887b8a6a0d', '2020-11-20 00:00:00', '2022-09-29 02:49:42', 'nacos', '0:0:0:0:0:0:0:1',
-        '', '', '代码生成', 'null', 'null', 'yaml', '', ''),
-       (7, 'ruoyi-job-dev.yml', 'DEFAULT_GROUP',
-        '# spring配置\nspring:\n  redis:\n    host: localhost\n    port: 6379\n    password: \n  datasource:\n    driver-class-name: com.mysql.cj.jdbc.Driver\n    url: jdbc:mysql://localhost:3306/ry-cloud?useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&useSSL=true&serverTimezone=GMT%2B8\n    username: root\n    password: password\n\n# mybatis配置\nmybatis:\n    # 搜索指定包别名\n    typeAliasesPackage: com.ruoyi.job.domain\n    # 配置mapper的扫描，找到所有的mapper.xml映射文件\n    mapperLocations: classpath:mapper/**/*.xml\n\n# swagger配置\nswagger:\n  title: 定时任务接口文档\n  license: Powered By ruoyi\n  licenseUrl: https://ruoyi.vip\n',
-        'edcf0e3fe13fea07b4ec08b1088f30b3', '2020-11-20 00:00:00', '2022-09-29 02:50:50', 'nacos', '0:0:0:0:0:0:0:1',
-        '', '', '定时任务', 'null', 'null', 'yaml', '', ''),
-       (8, 'ruoyi-file-dev.yml', 'DEFAULT_GROUP',
-        '# 本地文件上传    \r\nfile:\r\n    domain: http://127.0.0.1:9300\r\n    path: D:/ruoyi/uploadPath\r\n    prefix: /statics\r\n\r\n# FastDFS配置\r\nfdfs:\r\n  domain: http://8.129.231.12\r\n  soTimeout: 3000\r\n  connectTimeout: 2000\r\n  trackerList: 8.129.231.12:22122\r\n\r\n# Minio配置\r\nminio:\r\n  url: http://8.129.231.12:9000\r\n  accessKey: minioadmin\r\n  secretKey: minioadmin\r\n  bucketName: test',
-        '5382b93f3d8059d6068c0501fdd41195', '2020-11-20 00:00:00', '2020-12-21 21:01:59', NULL, '0:0:0:0:0:0:0:1', '',
-        '', '文件服务', 'null', 'null', 'yaml', NULL, ''),
-       (9, 'sentinel-ruoyi-gateway', 'DEFAULT_GROUP',
-        '[\r\n    {\r\n        \"resource\": \"ruoyi-auth\",\r\n        \"count\": 500,\r\n        \"grade\": 1,\r\n        \"limitApp\": \"default\",\r\n        \"strategy\": 0,\r\n        \"controlBehavior\": 0\r\n    },\r\n	{\r\n        \"resource\": \"ruoyi-system\",\r\n        \"count\": 1000,\r\n        \"grade\": 1,\r\n        \"limitApp\": \"default\",\r\n        \"strategy\": 0,\r\n        \"controlBehavior\": 0\r\n    },\r\n	{\r\n        \"resource\": \"ruoyi-gen\",\r\n        \"count\": 200,\r\n        \"grade\": 1,\r\n        \"limitApp\": \"default\",\r\n        \"strategy\": 0,\r\n        \"controlBehavior\": 0\r\n    },\r\n	{\r\n        \"resource\": \"ruoyi-job\",\r\n        \"count\": 300,\r\n        \"grade\": 1,\r\n        \"limitApp\": \"default\",\r\n        \"strategy\": 0,\r\n        \"controlBehavior\": 0\r\n    }\r\n]',
-        '9f3a3069261598f74220bc47958ec252', '2020-11-20 00:00:00', '2020-11-20 00:00:00', NULL, '0:0:0:0:0:0:0:1', '',
-        '', '限流策略', 'null', 'null', 'json', NULL, '');
+INSERT INTO `dimple-config`.config_info (id, data_id, group_id, content, md5, gmt_create, gmt_modified, src_user,
+                                         src_ip, app_name, tenant_id, c_desc, c_use, effect, type, c_schema,
+                                         encrypted_data_key)
+VALUES (1, 'application-dev.yml', 'DEFAULT_GROUP', 'spring:
+  autoconfigure:
+    exclude: com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure
+  mvc:
+    pathmatch:
+      matching-strategy: ant_path_matcher
+
+# feign 配置
+feign:
+  sentinel:
+    enabled: true
+  okhttp:
+    enabled: true
+  httpclient:
+    enabled: false
+  client:
+    config:
+      default:
+        connectTimeout: 10000
+        readTimeout: 10000
+  compression:
+    request:
+      enabled: true
+    response:
+      enabled: true
+
+# 暴露监控端点
+management:
+  endpoints:
+    web:
+      exposure:
+        include: \' *\'
+', 'aaa73b809cfd4d0058893aa13da57806', '2020-05-20 12:00:00', '2022-04-24 10:26:34', 'nacos', '0:0:0:0:0:0:0:1', '', '',
+        '通用配置', 'null', 'null', 'yaml', null, '');
+INSERT INTO `dimple-config`.config_info (id, data_id, group_id, content, md5, gmt_create, gmt_modified, src_user,
+                                         src_ip, app_name, tenant_id, c_desc, c_use, effect, type, c_schema,
+                                         encrypted_data_key)
+VALUES (2, 'dimple-gateway-dev.yml', 'DEFAULT_GROUP', 'spring:
+  redis:
+    host: dimple-redis
+    port: 6379
+    password: password
+  cloud:
+    gateway:
+      discovery:
+        locator:
+          lowerCaseServiceId: true
+          enabled: true
+      routes:
+        # 认证中心
+        - id: dimple-auth
+          uri: lb://dimple-auth
+          predicates:
+            - Path=/auth/**
+          filters:
+            # 验证码处理
+            - CacheRequestFilter
+            - ValidateCodeFilter
+            - StripPrefix=1
+        # 定时任务
+        - id: dimple-job
+          uri: lb://dimple-job
+          predicates:
+            - Path=/schedule/**
+          filters:
+            - StripPrefix=1
+        # 系统模块
+        - id: dimple-system
+          uri: lb://dimple-system
+          predicates:
+            - Path=/system/**
+          filters:
+            - StripPrefix=1
+        # 文件服务
+        - id: dimple-file
+          uri: lb://dimple-file
+          predicates:
+            - Path=/file/**
+          filters:
+            - StripPrefix=1
+
+# 安全配置
+security:
+  # 验证码
+  captcha:
+    enabled: true
+    type: math
+  # 防止XSS攻击
+  xss:
+    enabled: true
+    excludeUrls:
+      - /system/notice
+  # 不校验白名单
+  ignore:
+    whites:
+      - /auth/logout
+      - /auth/login
+      - /auth/register
+      - /*/v2/api-docs
+      - /csrf
+', '57cec5abd0e0a6b77d853750344a9dc0', '2020-05-14 14:17:55', '2022-09-29 02:48:32', 'nacos', '0:0:0:0:0:0:0:1', '', '',
+        '网关模块', 'null', 'null', 'yaml', '', '');
+INSERT INTO `dimple-config`.config_info (id, data_id, group_id, content, md5, gmt_create, gmt_modified, src_user,
+                                         src_ip, app_name, tenant_id, c_desc, c_use, effect, type, c_schema,
+                                         encrypted_data_key)
+VALUES (3, 'dimple-auth-dev.yml', 'DEFAULT_GROUP', 'spring:
+  redis:
+    host: dimple-redis
+    port: 6379
+    password: password
+', '8bd9dada9a94822feeab40de55efced6', '2020-11-20 00:00:00', '2022-09-29 02:48:42', 'nacos', '0:0:0:0:0:0:0:1', '', '',
+        '认证中心', 'null', 'null', 'yaml', '', '');
+INSERT INTO `dimple-config`.config_info (id, data_id, group_id, content, md5, gmt_create, gmt_modified, src_user,
+                                         src_ip, app_name, tenant_id, c_desc, c_use, effect, type, c_schema,
+                                         encrypted_data_key)
+VALUES (4, 'dimple-monitor-dev.yml', 'DEFAULT_GROUP', '# spring
+spring:
+  security:
+    user:
+      name: dimple
+      password: 123456
+  boot:
+    admin:
+      ui:
+        title: Dimple服务状态监控
+', '6f122fd2bfb8d45f858e7d6529a9cd44', '2020-11-20 00:00:00', '2022-09-29 02:48:54', 'nacos', '0:0:0:0:0:0:0:1', '', '',
+        '监控中心', 'null', 'null', 'yaml', '', '');
+INSERT INTO `dimple-config`.config_info (id, data_id, group_id, content, md5, gmt_create, gmt_modified, src_user,
+                                         src_ip, app_name, tenant_id, c_desc, c_use, effect, type, c_schema,
+                                         encrypted_data_key)
+VALUES (5, 'dimple-system-dev.yml', 'DEFAULT_GROUP', '# spring配置
+spring:
+  redis:
+    host: dimple-redis
+    port: 6379
+    password: password
+  datasource:
+    druid:
+      stat-view-servlet:
+        enabled: true
+        loginUsername: admin
+        loginPassword: 123456
+    dynamic:
+      druid:
+        initial-size: 5
+        min-idle: 5
+        maxActive: 20
+        maxWait: 60000
+        timeBetweenEvictionRunsMillis: 60000
+        minEvictableIdleTimeMillis: 300000
+        validationQuery: SELECT 1 FROM DUAL
+        testWhileIdle: true
+        testOnBorrow: false
+        testOnReturn: false
+        poolPreparedStatements: true
+        maxPoolPreparedStatementPerConnectionSize: 20
+        filters: stat,slf4j
+        connectionProperties: druid.stat.mergeSql\\=true;druid.stat.slowSqlMillis\\=5000
+      datasource:
+          # 主库数据源
+          master:
+            driver-class-name: com.mysql.cj.jdbc.Driver
+            url: jdbc:mysql://dimple-mysql:3306/dimple-cloud?useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&useSSL=true&serverTimezone=GMT%2B8
+            username: root
+            password: password
+          # 从库数据源
+          # slave:
+            # username:
+            # password:
+            # url:
+            # driver-class-name:
+
+# mybatis配置
+mybatis:
+    # 搜索指定包别名
+    typeAliasesPackage: com.dimple.system
+    # 配置mapper的扫描，找到所有的mapper.xml映射文件
+    mapperLocations: classpath:mapper/**/*.xml
+
+# swagger配置
+swagger:
+  title: 系统模块接口文档
+  license: Powered By Dimple
+  licenseUrl: https://www.bianxiaofeng.com.cn', '48e0ed4a040c402bdc2444213a82c910', '2020-11-20 00:00:00',
+        '2022-09-29 02:49:09', 'nacos', '0:0:0:0:0:0:0:1', '', '', '系统模块', 'null', 'null', 'yaml', '', '');
+INSERT INTO `dimple-config`.config_info (id, data_id, group_id, content, md5, gmt_create, gmt_modified, src_user,
+                                         src_ip, app_name, tenant_id, c_desc, c_use, effect, type, c_schema,
+                                         encrypted_data_key)
+VALUES (6, 'dimple-job-dev.yml', 'DEFAULT_GROUP', '# spring配置
+spring:
+  redis:
+    host: dimple-redis
+    port: 6379
+    password: password
+  datasource:
+    driver-class-name: com.mysql.cj.jdbc.Driver
+    url: jdbc:mysql://dimple-mysql:3306/dimple-cloud?useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&useSSL=true&serverTimezone=GMT%2B8
+    username: root
+    password: password
+
+# mybatis配置
+mybatis:
+    # 搜索指定包别名
+    typeAliasesPackage: com.dimple.job.domain
+    # 配置mapper的扫描，找到所有的mapper.xml映射文件
+    mapperLocations: classpath:mapper/**/*.xml
+
+# swagger配置
+swagger:
+  title: 定时任务接口文档
+  license: Powered By Dimple
+  licenseUrl: https://www.bianxiaofeng.com.cn
+', 'edcf0e3fe13fea07b4ec08b1088f30b3', '2020-11-20 00:00:00', '2022-09-29 02:50:50', 'nacos', '0:0:0:0:0:0:0:1', '', '',
+        '定时任务', 'null', 'null', 'yaml', '', '');
+INSERT INTO `dimple-config`.config_info (id, data_id, group_id, content, md5, gmt_create, gmt_modified, src_user,
+                                         src_ip, app_name, tenant_id, c_desc, c_use, effect, type, c_schema,
+                                         encrypted_data_key)
+VALUES (7, 'dimple-file-dev.yml', 'DEFAULT_GROUP', '# 本地文件上传
+file:
+    domain: http://127.0.0.1:9300
+    path: D:/dimple/uploadPath
+    prefix: /statics
+
+# FastDFS配置
+fdfs:
+  domain: http://8.129.231.12
+  soTimeout: 3000
+  connectTimeout: 2000
+  trackerList: 8.129.231.12:22122
+
+# Minio配置
+minio:
+  url: http://8.129.231.12:9000
+  accessKey: minioadmin
+  secretKey: minioadmin
+  bucketName: test', '5382b93f3d8059d6068c0501fdd41195', '2020-11-20 00:00:00', '2020-12-21 21:01:59', null,
+        '0:0:0:0:0:0:0:1', '', '', '文件服务', 'null', 'null', 'yaml', null, '');
+INSERT INTO `dimple-config`.config_info (id, data_id, group_id, content, md5, gmt_create, gmt_modified, src_user,
+                                         src_ip, app_name, tenant_id, c_desc, c_use, effect, type, c_schema,
+                                         encrypted_data_key)
+VALUES (8, 'sentinel-dimple-gateway', 'DEFAULT_GROUP', '[
+    {
+        "resource": "dimple-auth",
+        "count": 500,
+        "grade": 1,
+        "limitApp": "default",
+        "strategy": 0,
+        "controlBehavior": 0
+    },
+	{
+        "resource": "dimple-system",
+        "count": 1000,
+        "grade": 1,
+        "limitApp": "default",
+        "strategy": 0,
+        "controlBehavior": 0
+    },
+	{
+        "resource": "dimple-job",
+        "count": 300,
+        "grade": 1,
+        "limitApp": "default",
+        "strategy": 0,
+        "controlBehavior": 0
+    }
+]', '9f3a3069261598f74220bc47958ec252', '2020-11-20 00:00:00', '2020-11-20 00:00:00', null, '0:0:0:0:0:0:0:1', '', '',
+        '限流策略', 'null', 'null', 'json', null, '');
+
 
 
 /******************************************/
