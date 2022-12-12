e4996d0fd6e3f7540c631d9d55a454b9f62b0c0e
Martin <bianxiaofeng@sohu.com>
2022-12-12 11:08:54 +0800

    update sql

--- sql/dimple_20220814.sql
+++ sql/dimple_20220814.sql
@@ -1,3 +1,16 @@
+DROP
+DATABASE IF EXISTS `dimple-cloud`;
+
+CREATE
+DATABASE  `dimple-cloud` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+
+SET NAMES utf8mb4;
+SET
+FOREIGN_KEY_CHECKS = 0;
+
+USE
+`dimple-cloud`;
+
 SET NAMES utf8mb4;
 
 -- ----------------------------
