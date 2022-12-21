1840fdc8eca0634dd4a9d17307a7ca342ebe0160
Martin <bianxiaofeng@sohu.com>
2022-12-21 16:08:50 +0800

    fix register page background page path error

--- dimple-ui/src/views/register/index.vue
+++ dimple-ui/src/views/register/index.vue
@@ -152,7 +152,7 @@ export default {
   justify-content: center;
   align-items: center;
   height: 100%;
-  background-image: url("../assets/images/login-background.jpg");
+  background-image: url("../../assets/images/login-background.jpg");
   background-size: cover;
 }
 
