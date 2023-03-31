7e60e73c835333d26c0186a9cf58c68ec9e6d51d
Martin <bianxiaofeng@sohu.com>
2023-03-31 23:20:13 +0800

    update remote global config url

--- dimple-api/dimple-api-system/src/main/java/com/dimple/system/api/RemoteBlogService.java
+++ dimple-api/dimple-api-system/src/main/java/com/dimple/system/api/RemoteBlogService.java
@@ -15,6 +15,6 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @FeignClient(contextId = "remoteBlogGlobalConfigService", value = ServiceNameConstants.BLOG_SERVICE, fallbackFactory = RemoteDictFallbackFactory.class)
 public interface RemoteBlogService {
-    @GetMapping(value = "/blog/config/global")
+    @GetMapping(value = "/config/global")
     ResponseEntity<BlogGlobalConfig> getBlogGlobalConfig();
 }
